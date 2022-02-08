9d92d780bc0641c766116221d764303b8720d47e
Emeric Coude <emericmax9@hotmail.com>
2022-02-08 17:19:48 -0500

    Crumbling Platform Anim Speed
    - Added a crumbleSpeed variable to the Crumbling platform (which will change the animation speed of it falling).

--- Assets/Emeric-Dev/PlatformCrumbling.cs
+++ Assets/Emeric-Dev/PlatformCrumbling.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PlatformCrumbling : Platform
 {
     [SerializeField] Animator animator;
+    [Tooltip("The delay before the leaf starts crumbling once the player has landed on it.")]
     [SerializeField] float crumbleDelay = 0.5f;
+    [Tooltip("The delay before the leaf resets to its normal position.")]
     [SerializeField] float rechargeDelay = 2f;
+    [Tooltip("The speed at which the leaf crumbles. This is set on awake, so changing it at runtime will not affect change the speed.")]
+    [SerializeField] float crumbleSpeed = 1f;
     bool recharging = false;
     
+    void Awake()
+    {
+        animator.speed = crumbleSpeed;
+    }
+
     protected override void OnPlayerLand(GameObject player)
     {
         if (!recharging)

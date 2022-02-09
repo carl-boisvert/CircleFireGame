4094b9e308fded9544378ff99e8a04bd347ec339
Emeric Coude <emericmax9@hotmail.com>
2022-02-08 22:51:59 -0500

    UI 0.0.1
    - Added a MainMenu scene;
    - Imported TextMeshPro;
    - Re-organized some of my folder;
    - Imported Bryan's font;
    - Started setting up UI;
    - Added a SceneManager script with transition.

---
+++ Assets/Emeric-Dev/Scripts/Scene_Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    [SerializeField] GameObject fadeIn;
+    [SerializeField] float transitionDuration = 2f;
+
+    public void ChangeToScene(string sceneName){
+        StartCoroutine(ChangeToSceneCoroutine(sceneName));
+    }
+    
+    IEnumerator ChangeToSceneCoroutine(string sceneName){
+        Instantiate(fadeIn, Vector3.zero, Quaternion.identity);
+        yield return new WaitForSeconds(transitionDuration);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApplication(){
+        Application.Quit();
+    }
+    //DELETE. Test commit - Justin
+}

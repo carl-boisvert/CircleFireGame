319b0d2a229756b7f7574ef2a76fba8af460bb63
Emeric Coude <emericmax9@hotmail.com>
2022-02-16 19:51:15 -0500

    Audio Player functionalities & Bounce Mushroom sounds
    - Added some functionalities to my audioPlayer scritp such as PlayAudioClipsFromRange and PlayAudioClipRandomFromRange.
    - Added audio to the Bouncing Shrooms.

--- Assets/Emeric-Dev/Scripts/PlatformBouncing.cs
+++ Assets/Emeric-Dev/Scripts/PlatformBouncing.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class PlatformBouncing : Platform
 {
-    [SerializeField] float force = 1f;
+    [SerializeField] float _bounceforce = 1f;
+
+    [Header("Audio")]
+    [SerializeField] Audio_AudioPlayer _audioPlayer;
+    [SerializeField] Vector2Int _audioIndexRange = new Vector2Int(0, 1);
+    
+    void Awake()
+    {
+        if (_audioPlayer == null) { this.gameObject.TryGetComponent<Audio_AudioPlayer>(out _audioPlayer); }
+    }
     
     public override void OnPlayerLand()
     {
         if (player.TryGetComponent<Avatar>(out Avatar playerAvatar)){
-            playerAvatar.SetVelocityY(force);
+            playerAvatar.SetVelocityY(_bounceforce);
+            _audioPlayer.PlayAudioClipsFromRange(_audioIndexRange.x, _audioIndexRange.y);
         }
     }
 }

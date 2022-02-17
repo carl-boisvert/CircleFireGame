fedc4df2602371435a2129074b75ac6008f1380f
Emeric Coude <emericmax9@hotmail.com>
2022-02-16 21:28:51 -0500

    More sound stuff

--- Assets/Emeric-Dev/Scripts/PlatformBouncing.cs
+++ Assets/Emeric-Dev/Scripts/PlatformBouncing.cs
@@ -8,7 +8,6 @@ public class PlatformBouncing : Platform
 
     [Header("Audio")]
     [SerializeField] Audio_AudioPlayer _audioPlayer;
-    [SerializeField] Vector2Int _audioIndexRange = new Vector2Int(0, 1);
     
     void Awake()
     {
@@ -19,7 +18,7 @@ public class PlatformBouncing : Platform
     {
         if (player.TryGetComponent<Avatar>(out Avatar playerAvatar)){
             playerAvatar.SetVelocityY(_bounceforce);
-            _audioPlayer.PlayAudioClipsFromRange(_audioIndexRange.x, _audioIndexRange.y);
+            _audioPlayer.PlayAudioClip(0);
         }
     }
 }

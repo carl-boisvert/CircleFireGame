3d0bc5c923c801ef6d79fea93100eb810e1512ac
Emeric Coude <emericmax9@hotmail.com>
2022-02-21 02:19:57 -0500

    level-0 unlockable effects and some fixes

--- Assets/Emeric-Dev/Scripts/LightUnlocks.cs
+++ Assets/Emeric-Dev/Scripts/LightUnlocks.cs
@@ -8,12 +8,18 @@ public class LightUnlocks : MonoBehaviour
     [SerializeField] List<GameObject> lightsToEnable = new List<GameObject>();
     int currentLightIndex = 0;
 
+    [Header("Audio")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && PowerCell.amountHeld > 0){
             for(int i = 0; i < PowerCell.amountHeld; i++){
                 lightsToEnable[currentLightIndex].SetActive(true);
                 currentLightIndex++;
+
+                audioSource.PlayOneShot(audioClip);
             }
 
             PowerCell.amountHeld = 0;
